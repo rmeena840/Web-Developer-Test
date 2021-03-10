ef6de4e9f456277844792ddb386f2c9375071982
Ravinda <ravindra.iitism@gmail.com>
2021-03-10 20:10:47 +0530

    IconButton component and style added

---
+++ src/scripts/components/ui/IconButton.js
@@ -0,0 +1,21 @@
+// Icon Button component
+
+import React from "react";
+import "./IconButton.scss";
+
+function IconButton(props) {
+  return (
+    <button
+      className="IconButton"
+      disabled={props.isDisabled}
+      onClick={props.onClickHandler}
+      aria-label={props.label}
+      style={{
+        WebkitMaskImage: `url("${props.mask}"`,
+        maskImage: `url("${props.mask}"`,
+      }}
+    ></button>
+  );
+}
+
+export default IconButton;
