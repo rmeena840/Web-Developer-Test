a1dbd650aa383c5872b4d4ba8dad031bc4408f4a
Ravinda <ravindra.iitism@gmail.com>
2021-03-10 20:08:13 +0530

    Header component and style added

---
+++ src/scripts/components/layout/TopBar.js
@@ -0,0 +1,49 @@
+// Header component
+
+import React from "react";
+import "./TopBar.scss";
+import Menu from "./Menu";
+
+// Menu items list
+const menuItems = [
+  {
+    label: "Products",
+    href: "#",
+  },
+  {
+    label: "News",
+    href: "#",
+  },
+  {
+    label: "Contact",
+    href: "#",
+  },
+  {
+    label: "Cart",
+    href: "#",
+    iconUrl: "/static/images/cart.svg",
+  },
+];
+
+function TopBar() {
+  return (
+    <div className="TopBar">
+      <div className="TopBar__container">
+        <div className="TopBar__left">
+          <a href="/" aria-label="Back home">
+            <img
+              className="TopBar__logo"
+              src="/static/images/logo.svg"
+              alt="Logo Apps"
+            />
+          </a>
+        </div>
+        <div className="TopBar__right">
+          <Menu />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopBar;
