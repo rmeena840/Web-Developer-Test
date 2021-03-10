ce4eaab005678f236a27439514875484c4ab204b
Ravinda <ravindra.iitism@gmail.com>
2021-03-10 20:09:28 +0530

    Button component and style added

---
+++ src/scripts/components/ui/Button.js
@@ -0,0 +1,18 @@
+// Buy Now button component
+import React from "react";
+import "./Button.scss";
+
+function Button(props) {
+  return (
+    <button
+      className={`Button ${props.hasSpinner ? "has-spinner" : ""}`}
+      disabled={props.isDisabled}
+      onClick={props.onClickHandler}
+    >
+      <span className="Button__label">Buy Now</span>
+      {props.hasSpinner && <span className="Button__spinner"></span>}
+    </button>
+  );
+}
+
+export default Button;
