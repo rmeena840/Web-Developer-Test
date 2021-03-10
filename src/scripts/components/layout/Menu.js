045c0b48557028c284499437c3e67e88dd45d774
Ravinda <ravindra.iitism@gmail.com>
2021-03-10 20:04:52 +0530

    Menu component added

---
+++ src/scripts/components/layout/Menu.js
@@ -0,0 +1,46 @@
+// Menu component
+
+import React, { useState } from "react";
+import { useMediaQuery } from "@react-hook/media-query";
+
+import "./Menu.scss";
+
+import Portal from "./../Portal";
+import MenuList from "./MenuList";
+
+function Menu() {
+  const layoutBreakpointMatches = useMediaQuery(
+    "only screen and (min-width: 1024px)"
+  );
+  const [mobileIsVisible, setMobileIsVisible] = useState(false);
+
+  function toggleVisibility() {
+    setMobileIsVisible(!mobileIsVisible);
+  }
+
+  if (layoutBreakpointMatches) {
+    return (
+      <nav className="Menu">
+        <MenuList type="desktop" />
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="Menu">
+        <button className="Menu__toggle" onClick={toggleVisibility}>
+          <span className="Menu__toggle-icon"></span>
+          <span className="Menu__toggle-label">Menu</span>
+        </button>
+
+        {mobileIsVisible && (
+          <Portal>
+            <span className="Menu__backdrop" onClick={toggleVisibility}></span>
+            <MenuList type="mobile" />
+          </Portal>
+        )}
+      </nav>
+    );
+  }
+}
+
+export default Menu;
---
+++ src/scripts/components/layout/MenuList.js
@@ -0,0 +1,68 @@
+// Menu list with hperlinks
+
+import React, { Fragment, useContext } from "react";
+import "./MenuList.scss";
+
+import CartContext from "./../../pages/cart/state/CartContext";
+
+const menuItems = [
+  {
+    label: "Products",
+    href: "#",
+  },
+  {
+    label: "News",
+    href: "#",
+  },
+  {
+    label: "Contact",
+    href: "#",
+  },
+  {
+    label: "Cart",
+    href: "#",
+    iconUrl: "/static/images/cart.svg",
+  },
+];
+
+function MenuList(props) {
+  const { state, dispatch } = useContext(CartContext);
+  return (
+    <ul className={`MenuList ${props.type}`}>
+      {menuItems.map((item, index) => (
+        <li className="MenuList__item" key={index}>
+          <a
+            className={`MenuList__link ${
+              item.iconUrl ? "is-icon" : "is-label"
+            }`}
+            href={item.href}
+            aria-label={item.label}
+          >
+            {item.iconUrl ? (
+              <Fragment>
+                <span
+                  className="MenuList__link-icon"
+                  style={{
+                    WebkitMaskImage: `url(${item.iconUrl})`,
+                    maskImage: `url(${item.iconUrl})`,
+                  }}
+                >
+                  {item.label}
+                </span>
+                {state.items.length > 0 && (
+                  <span className="MenuList__link-icon-number">
+                    {state.items.length}
+                  </span>
+                )}
+              </Fragment>
+            ) : (
+              <span className="MenuList__link-label">{item.label}</span>
+            )}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default MenuList;
